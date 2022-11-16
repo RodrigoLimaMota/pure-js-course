ff21c6b0ef35e8f827344ae3733aa9cd36beb7e5
Rodrigo <rodrigo.0743@gmail.com>
2022-11-16 17:31:19 -0300

    solve and refactor exercises 01 to 05

--- solved-exercises/exercicio-37/app.js
+++ solved-exercises/exercicio-37/app.js
@@ -1,41 +1,56 @@
 /*
   01
-
   - Descomente a let abaixo, descubra o que o código está tentando fazer e 
     faça-o funcionar.
 */
 
 class Animal {
-  constructor (name) {
+  constructor(name) {
     this.name = name
   }
 }
 
 class Rabbit extends Animal {
-  constructor (name) {
-    this.name = name
-    this.created = new Date()
-  }
+  created = new Date()
 }
 
-// let rabbit = new Rabbit('White Rabbit')
+let rabbit = new Rabbit('White Rabbit')
+// console.log(rabbit)
 
 /*
   02
-
   - Descomente o código abaixo e implemente o que está faltando para que ele 
     funcione.
 */
 
-// const counter = new Counter()
+class Counter {
 
-// counter.getValue()
+  #value = 0
+
+  get value() {
+    return this.#value
+  }
+
+  set newValue(newValue) {
+    this.#value = newValue
+  }
+
+  increment() {
+    return ++this.#value
+  }
+}
+
+const counter = new Counter()
+
+// console.log(counter.value)
 // counter.increment()
-// counter.getValue()
+// console.log(counter.value)
+// counter.newValue = 91
+// console.log(counter.value)
+
 
 /*
   03
-
   - A partir do array abaixo, gere um novo array com apenas os valores truthy;
   - Utilize um construtor para resolver este exercício;
   - Não invoque o construtor.
@@ -47,12 +62,14 @@ const values = [
   '',
   [],
   NaN,
-  () => {}
+  () => { }
 ]
 
-/*
-  04
+const truthyValues = values.filter(Boolean)
+// console.log(truthyValues)
 
+/*
+    04
   - O código abaixo deveria exibir no console, à cada segundo, uma string com 
     as horas minutos e segundos, no seguinte formato: "h:m:s" onde "h" 
     representa as horas, "m" os minutos e "s" os segundos. Exemplo: "22:01:25";
@@ -60,78 +77,83 @@ const values = [
     funcione.
 */
 
-// class Clock {
-//   constructor ({ template }) {
-//     this.template = template
-//   }
+const getTimeUnits = () => {
 
-//   render () {
-//     const date = new Date()
-//     let hours = date.getHours()
-//     let minutes = date.getMonth()
-//     let seconds = date.getSeconds()
+  const date = new Date()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
 
-//     if (hours < 10) {
-//       hours = `0${hours}`
-//     }
+  return [hours, minutes, seconds]
+}
 
-//     if (minutes < 10) {
-//       minutes = `0${minutes}`
-//     }
+const formatTimeUnits = units => units
+  .map(unit => unit < 10 ? unit = `0${unit}` : unit)
 
-//     if (seconds < 10) {
-//       seconds = `0${seconds}`
-//     }
+const getFormattedTime = template => {
 
-//     const formattedTime = this.template
-//       .replace('h', hours)
-//       .replace('m', minutes)
-//       .replace('s', seconds)
+  const timeUnits = getTimeUnits()
 
-//     console.log(formattedTime)
-//   }
+  const FormattedTime = formatTimeUnits(timeUnits)
 
-//   start () {
-//     this.render()
-//     this.timer = setInterval(() => this.render(), 1000)
-//   }
+  return template
+    .split(':')
+    .map((_, index) => FormattedTime[index])
+    .join(':')
+}
 
-//   stop () {
-//     clearInterval(this.timer)
-//   }
-// }
+class Clock {
+  constructor({ template }) {
+    this.template = template
+  }
 
-// class ExtendedClock extends Clock {
-//   constructor ({ options }) {
-//     super(options)
-    
-//     let { precision = 1000 } = options
-//     this.precision = precision
-//   }
+  render() {
+    const formattedTime = getFormattedTime(this.template)
+    console.log(formattedTime)
+  }
+}
 
-//   start () {
-//     this.render()
-//     this.timer = setInterval(() => this.render(), this.precision)
-//   }
-// }
+class ExtendedClock extends Clock {
+  constructor(options) {
+    super(options)
 
-// const clock = ExtendedClock({ template: 'h:m:s', precision: 1000 })
+    const { precision = 1000 } = options
+    this.oneSecond = precision
+  }
+
+  start() {
+    this.render()
+    this.timer = setInterval(() => this.render(), this.oneSecond)
+  }
+}
+
+const clock = new ExtendedClock({ template: 'h:m:s', precision: 1000 })
 
 // clock.start()
 
+
 /*
   05
-
   - No index.html há um elemento "textarea" e um parágrafo. A cada vez que um 
     caractere for inserido no textarea, exiba no parágrafo a quantidade de 
     caracteres que o textarea contém.
 */
 
+const textArea = document.querySelector(`[data-js="textarea"]`)
+const paragraph = document.querySelector(`[data-js="paragraph"]`)
 
+const displayCharactersCounter = event => {
+
+  const amountOfCharacters = event.target.value.length
+  const lengthOfTextArea = textArea.getAttribute('maxlength')
+
+  paragraph.textContent = `${amountOfCharacters}/${lengthOfTextArea}`
+}
+
+textArea.addEventListener('input', displayCharactersCounter)
 
 /*
   06
-
   - Já implementamos os métodos forEach, some, map e filter, do zero;
   - Neste exercício, seu desafio será criar, do zero, o método reduce;
   - Implemente uma função "reduce" que possui a mesma funcionalidade do método 
@@ -155,3 +177,4 @@ const values = [
     vídeo de correção dos exercícios um link para a aula de introdução ao 
     reduce e um link para a documentação do método no MDN.
 */
+
